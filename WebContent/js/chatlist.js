5a4c11a891583c4e43875f854eb16ddfd7fcd340
HyungTaeKim8905 <75460537+HyungTaeKim8905@users.noreply.github.com>
2020-12-13 23:03:13 +0900

    AddFriend() 함수에 ajax 기능 추가
    AddFriend() 함수에 ajax 기능 추가

--- WebContent/js/chatlist.js
+++ WebContent/js/chatlist.js
@@ -22,7 +22,7 @@ function FindTest()	{
 				alert("친구목록을 가져오지 못했다.");
 				NoFriend();
 			}
-			alert(json[0]["statusmessage"]);
+			//alert(json[0]["statusmessage"]);
 			for(var i = 0; json.length; i++)	{
 				SuccessFriend(json[i]["ID"], json[i]["pictureRealName"], json[i]["statusmessage"]);
 			}
@@ -48,8 +48,8 @@ function SuccessFriend(ID, pictureRealName, statusmessage) {
 	output += 	"</div>";
 	output += "</div>";
 	output += "<hr>";
-	$("#div2").html(output);
-} 
+	$("#div3").html(output);
+}
 */
 
 function SuccessFriend(ID, pictureRealName, statusmessage)	{
@@ -58,24 +58,48 @@ function SuccessFriend(ID, pictureRealName, statusmessage)	{
 	output += 		"<tr>";
 	output +=			"<td><a href='#' style='padding-top:0px; padding-bottom:0px;'><img src='" + pictureRealName + "' style='width:55px; height:50px; float:left;'></a></td>";
 	output +=			"<td style='width:72%;'><h4>" + statusmessage + "</h4></td>";
-	output +=			"<td><button type='button' onclick='AddFriend(" + ID + ");' style='width:55px;'>친구추가</button></td>";
+	output +=			"<td><button type='button' onclick='AddFriend(" + ID + ")' style='width:55px;'>친구추가</button></td>";
 	output += 		"</tr>";
 	output += 		"<tr>";
 	output +=			"<td>&nbsp&nbsp&nbsp" + ID + "</td>";
 	output += 		"</tr>";
 	output += "</table>";
 	output == "<hr>";
-	$("#div2").append(output);
+	$("#div3").append(output);
 }
 
 function NoFriend()	{
 	var output = "";
 	output += "<h5>검색 결과를 얻어오지 못했습니다.</h5>";
-	$("#div2").html(output);
+	$("#div3").html(output);
 }
 
-function AddFriend(ID)	{
-	alert(ID);
-	//ajax
+function AddFriend(AddID)	{
+	alert(AddID);
+	//알럿이 안뜸 해결 바람
+	var check = confirm(AddID + "님을 친구추가 하시겠습니까?");
+	if(check == true)	{
+		alert("확인 버튼 눌렀다.");
+	}
 	
+	if(check == false)	{
+		alert("취소 버튼 눌렀다.");
+		return false;
+	}
+	$.ajax({ 
+		url:"#",
+		type:"POST",
+		data:{AddID:AddID},
+		success:function(result){
+			var json = JSON.parse(result);
+			if(json == null)	{
+				alert("친구목록을 가져오지 못했다.");
+				NoFriend();
+			}
+			alert(json[0]["statusmessage"]);
+			for(var i = 0; json.length; i++)	{
+				SuccessFriend(json[i]["ID"], json[i]["pictureRealName"], json[i]["statusmessage"]);
+			}
+		}
+	});
 }
